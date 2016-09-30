3603f2d7b6307cdde8981791a4a612e5524162f6
Edd Figueiredo <eddsilva@gmail.com>
2016-09-30 16:11:54 -0300

    added route to show certificates

--- routes/web.php
+++ routes/web.php
@@ -19,13 +19,21 @@ Route::get('/import-cert', function () {
     return view('import_certificate');
 });
 
+Route::get('/export-cert', function () {
+    return view('export_certificate');
+});
+
 Route::get('/new-cert', function () {
     return view('new_certificate');
 });
 
-Route::get('/show-certs', function () {
+Route::get('/show-certs', function(){
     return view('show_certs');
 });
+
+Route::get('/get-certs', [
+    'uses' => 'CertificateController@showCertificates'
+]);
 
 Route::post('/create-certificate', [
 	'uses' => 'CertificateController@newCertificate',
@@ -35,4 +43,9 @@ Route::post('/create-certificate', [
 Route::post('/import-certificate', [
 	'uses' => 'CertificateController@importCertificate',
 	'as' => 'import_certificate'
+]);
+
+Route::post('/export-certificate', [
+    'uses' => 'CertificateController@exportCertificate',
+    'as' => 'export_certificate'
 ]);
