e7f19377dd7838e102cee68b9cd217aae8a43687
EddFigueiredo <eddsilva@gmail.com>
2016-10-07 17:41:49 -0300

    Created custom command to send slack notifications about expiring certificates

---
+++ app/Console/Commands/SendSlackNotificationAboutCertificateExpiration.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Notifications\CertificateExpiring;
+
+class SendSlackNotificationAboutCertificateExpiration extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'SendSlackNotificationAboutCertificateExpiration:send';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Send Slack notification about certificates that are about to expire.';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $certificates = \App\Certificate::all();
+
+        foreach ($certificates as $key => $certificate) {
+            $expiration = date_create($certificate['expiration']);
+            $today = date_create("now");
+            $diff = date_diff($today,$expiration);
+
+            if ($diff->format("%a") < 374) {
+                $certificate->notify(new CertificateExpiring($certificate));
+            }
+        } 
+    }
+}
